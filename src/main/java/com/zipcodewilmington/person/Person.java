f1f62cf1229c693b0838a6f16e8d803ac855a428
Anthony Jordan <anthonyjordan@zipcoders-MacBook-Pro-10.local>
2018-02-12 21:46:25 -0500

    Finished project

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,82 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String hairColor;
+    private String eyeColor;
+    private boolean hasGlasses;
+    private String height;
+    private String weight;
+
 
     public Person() {
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getHairColor() {
+        return this.hairColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getEyeColor() {
+        return this.eyeColor;
+    }
+
+    public void setHasGlasses(boolean hasGlasses) {
+        this.hasGlasses = hasGlasses;
+    }
+
+    public boolean getHasGlasses() {
+        return this.hasGlasses;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getHeight(){
+        return this.height;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getWeight(){
+        return this.weight;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -9,8 +9,8 @@ import org.junit.Test;
 public class TestPerson {
     @Test
     public void testDefaultConstructor() {
-        String expectedName = "";
-        Integer expectedAge = Integer.MAX_VALUE;
+        String expectedName = null;
+        Integer expectedAge = 0;
 
         Person person = new Person();
 
@@ -22,7 +22,7 @@ public class TestPerson {
     }
 
     @Test
-    public void testConstructorWithName(){
+    public void testConstructorWithName() {
         String expected = "Leon";
         Person person = new Person(expected);
         String actual = person.getName();
@@ -30,20 +30,17 @@ public class TestPerson {
     }
 
     @Test
-    public void testConstructorWithAge(){
+    public void testConstructorWithAge() {
         Integer expected = 5;
         Person person = new Person(expected);
         person.setAge(expected);
-        String actual = person.getName();
+        Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
 
 
-
-
-
     @Test
-    public void testConstructorWithNameAndAge(){
+    public void testConstructorWithNameAndAge() {
         Integer expectedAge = 5;
         String expectedName = "Leon";
 
@@ -71,6 +68,51 @@ public class TestPerson {
         Integer expected = 5;
         person.setAge(expected);
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHairColor() {
+        Person person = new Person();
+        String expected = "blue";
+        person.setHairColor(expected);
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetEyeColor() {
+        Person person = new Person();
+        String expected = "Red";
+        person.setEyeColor(expected);
+        String actual = person.getEyeColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHasGlasses() {
+        Person person = new Person();
+        boolean expected = false;
+        person.setHasGlasses(expected);
+        boolean actual = person.getHasGlasses();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHeight() {
+        Person person = new Person();
+        String expected = "4' 9\"";
+        person.setHeight(expected);
+        String actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetWeight() {
+        Person person = new Person();
+        String expected = "85 pounds";
+        person.setWeight(expected);
+        String actual = person.getWeight();
         Assert.assertEquals(expected, actual);
     }
 }
