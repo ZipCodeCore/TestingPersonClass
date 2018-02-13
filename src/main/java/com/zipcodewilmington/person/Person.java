e70f11aec6bb435af80e9df178fb1a7f1debd650
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-2.local>
2018-02-13 14:55:06 -0500

    Finalizing Testing Person Class

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,89 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private  String socialSecurityNumber;
+    private String phoneNumber;
+    private String nationality;
+    private Double height;
+    private String maritalStatus;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age= age;
+    }
+    public Person(String socialSecurityNumber,String phoneNumber,String nationality,Double height,String maritalStatus){
+        this.socialSecurityNumber=socialSecurityNumber;
+        this.phoneNumber = phoneNumber;
+        this.nationality=nationality;
+        this.height=height;
+        this.maritalStatus=maritalStatus;
+
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
+
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return age;
+    }
+    public void setSocialSecurity(String socialSecurity) {
+        this.socialSecurityNumber = socialSecurity;
+    }
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+    public void setMaritalStatus(String status) {
+        this.maritalStatus = status;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Double getHeight() {
+        return height;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -67,6 +67,33 @@ public class TestPerson {
         Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedName, actualName);
     }
+    @Test
+    public void testConstructorWithFiveParameters() {
+        // Given
+        //String socialSecurityNumber,String phoneNumber,String nationality,Integer height,String maritalStatus
+        String expectedSocialSecurityNumber = "2003336545";
+        String expectedPhoneNumber = "2021112222";
+        String expectedNationality = "Eritrea";
+        Double expectedHeight = 6.0;
+        String expectedMaritalStatus = "Single";
+
+        // When
+        Person person = new Person(expectedSocialSecurityNumber, expectedPhoneNumber,expectedNationality,expectedHeight,expectedMaritalStatus);
+
+        // Then
+        String actualSocialSecurityNumber = person.getSocialSecurityNumber();
+        String actualPhoneNumber = person.getPhoneNumber();
+        String actualNationality = person.getNationality();
+        Double actualHeight = person.getHeight();
+        String actualMaritalStatus= person.getMaritalStatus();
+
+
+        Assert.assertEquals(expectedSocialSecurityNumber, actualSocialSecurityNumber);
+        Assert.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+        Assert.assertEquals(expectedNationality, actualNationality);
+        Assert.assertEquals(expectedHeight, actualHeight);
+        Assert.assertEquals(expectedMaritalStatus, actualMaritalStatus);
+    }
 
     @Test
     public void testSetName() {
@@ -95,4 +122,57 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void testSetSocialSecurity() {
+        // Given
+        Person person = new Person();
+        String expected = "5556667777";
+
+        // When
+        person.setSocialSecurity(expected);
+
+        // Then
+        String actual = person.getSocialSecurityNumber();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetPhoneNumber() {
+        // Given
+        Person person = new Person();
+        String expected = "3021119999";
+
+        // When
+        person.setPhoneNumber(expected);
+
+        // Then
+        String actual = person.getPhoneNumber();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetNationality() {
+        // Given
+        Person person = new Person();
+        String expected = "American";
+
+        // When
+        person.setSocialSecurity(expected);
+
+        // Then
+        String actual = person.getNationality();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        Double expected = 5.9;
+
+        // When
+        person.setHeight(expected);
+
+        // Then
+        Double actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+    }
+
 }
