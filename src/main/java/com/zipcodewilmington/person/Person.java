b08c4396c652553d016f19ca329353c36a3c67fe
Garrett Arant <garrettarant@MacBook-Pro-4.local>
2018-02-13 12:10:29 -0500

    Garrett Completed Lab

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,93 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String pet;
+    private String hairColor;
+    private String favoriteSport;
+    private String whereTheyLive;
+    private String whereTheyWork;
+
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
+        pet = "";
+        hairColor= "";
+        favoriteSport= "";
+        whereTheyLive= "";
+        whereTheyWork= "";
+
+
+
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
-    public Person(String name) {
+    public Person(String name){
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
     }
 
+
     public void setName(String name) {
+        this.name = name;
+
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
+    public void setPet(String pet) {
+        this.pet = pet;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public void setFavoriteSport(String favoriteSport) {
+        this.favoriteSport = favoriteSport;
+    }
+
+    public void setWhereTheyLive(String whereTheyLive) {
+        this.whereTheyLive = whereTheyLive;
+    }
+
+    public void setWhereTheyWork(String whereTheyWork) {
+        this.whereTheyWork = whereTheyWork;
+    }
+
+
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
+
+    public String getPet() { return this.pet; }
+
+    public String getHairColor() {
+        return this.hairColor;
+    }
+
+    public String getFavoriteSport() {
+        return this.favoriteSport;
+    }
+
+    public String getWhereTheyLive() {
+        return this.whereTheyLive;
+    }
+
+    public String getWhereTheyWork() { return this.whereTheyWork;
+    }
+
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -95,4 +95,78 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSetPet() {
+        // Given
+        Person person = new Person();
+        String expected = "Jack Russell";
+
+        // When
+        person.setPet(expected);
+        String actual = person.getPet();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHairColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Brown";
+
+        // When
+        person.setHairColor(expected);
+        String actual = person.getHairColor();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetFavoriteSport() {
+        // Given
+        Person person = new Person();
+        String expected = "Football";
+
+        // When
+        person.setFavoriteSport(expected);
+        String actual = person.getFavoriteSport();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetWhereTheyLive() {
+        // Given
+        Person person = new Person();
+        String expected = "Newark, Delaware";
+
+        // When
+        person.setWhereTheyLive(expected);
+        String actual = person.getWhereTheyLive();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetWhereTheyWork() {
+        // Given
+        Person person = new Person();
+        String expected = "Wilmington, Delaware";
+
+        // When
+        person.setWhereTheyWork(expected);
+        String actual = person.getWhereTheyWork();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
+
 }
