28717db2d5df88bc53e2f47c65aaea6a35e45290
CWinarski <Winarski70@yahoo.com>
2018-02-12 23:57:21 -0500

    Finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,99 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String hairColor;
+    private String eyeColor;
+    private String gender;
+    private double weight;
+    private double height;
+
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
+        hairColor = "";
+        eyeColor = "";
+        gender = "";
+        weight = Double.MAX_VALUE;
+        height = Double.MIN_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
+
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public Person (String hairColor, String eyeColor, String gender, double weight, double height){
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
     }
 }
