9bc6a1486f7171516af506c43a9e242670f62d27
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-13 20:27:48 -0500

    1st iteration. nothing to see here

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,48 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
+
     private String name;
     private int age;
 
     public Person() {
-    }
 
+    }
+    //constructor
     public Person(int age) {
-    }
 
+        this.age = age;
+    }
+    //constructor
     public Person(String name) {
-    }
 
+        this.name= name;
+    }
+    //constructor
     public Person(String name, int age) {
-    }
 
+        this.name = name;
+        this.age = age;
+    }
+    //mutator
     public void setName(String name) {
-    }
 
+        this.name = name;
+
+    }
+    //constructor
     public void setAge(int age) {
+        this.age = age;
+
+
     }
 
-    public String getName() {
-        return null;
+    public String getName(String name) {
+        return name;
     }
 
-    public Integer getAge() {
-        return null;
+    public Integer getAge(int age) {
+        return age;
     }
+
 }
