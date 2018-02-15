59d6c6c9e239c238e86804c4c38421b120167d8d
Jordan Elderidge <jordanelderidge@zipcoders-MacBook-Pro.local>
2018-02-15 15:38:14 -0500

    m

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,56 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int weight;
+    private String gender;
+    private int height;
+    private String hairColor;
+    private String nationality;
 
     public Person() {
+        name ="";
+        age =Integer.MAX_VALUE;
     }
 
-    public Person(int age) {
+    public Person(int newage) {
+        this.age = newage;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+    public int getWeight(){
+        return this.weight;
+    }
+    public String getGender(){
+        return this.gender;
     }
 
     public void setName(String name) {
+        this.name =name;
+
+
     }
+
 
     public void setAge(int age) {
+        this.age =age;
     }
 
-    public String getName() {
-        return null;
-    }
 
-    public Integer getAge() {
-        return null;
-    }
+
+
+
 }
