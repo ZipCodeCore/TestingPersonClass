2ccc69e43f1307772689e0ace91797de460cced4
Dominique Clarke <dominiquedclarke@gmail.com>
2018-03-26 13:00:23 -0400

    first commit

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,73 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int heightInInches;
+    private int weightInPounds;
+    private String favoriteFood;
+    private String occupation;
+    private String ethnicity;
 
     public Person() {
+        this.age = Integer.MAX_VALUE;
+        this.name = "";
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeightInInches(int height) {
+        this.heightInInches = height;
+    }
+
+    public void setWeightInPounds(int weight) {
+        this.weightInPounds = weight;
+    }
+
+    public void setFavoriteFood(String food) {
+        this.favoriteFood = food;
+    }
+
+    public void setOccupation(String job) {
+        this.occupation = job;
+    }
+
+    public void setEthnicity(String ethnicity) {
+        this.ethnicity = ethnicity;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
+
+    public Integer getHeightInInches() { return this.heightInInches; }
+
+    public Integer getWeightInPounds() { return this.weightInPounds; }
+
+    public String getFavoriteFood() { return this.favoriteFood; }
+
+    public String getOccupation() { return this.occupation; }
+
+    public String getEthnicity() { return this.ethnicity; }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,6 +12,11 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
+        Integer expectedHeight = 0;
+        Integer expectedWeight = 0;
+        String expectedFavoriteFood = null;
+        String expectedOccupation = null;
+        String expectedEthnicity = null;
 
         // When
         Person person = new Person();
@@ -19,9 +24,19 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        Integer actualHeight = person.getHeightInInches();
+        Integer actualWeight = person.getWeightInPounds();
+        String actualFavoriteFood = person.getFavoriteFood();
+        String actualOccupation = person.getOccupation();
+        String actualEthnicity = person.getEthnicity();
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedHeight, actualHeight);
+        Assert.assertEquals(expectedWeight, actualWeight);
+        Assert.assertEquals(expectedFavoriteFood, actualFavoriteFood);
+        Assert.assertEquals(expectedOccupation, actualOccupation);
+        Assert.assertEquals(expectedEthnicity, actualEthnicity);
     }
 
     @Test
@@ -93,6 +108,76 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHeightInInches() {
+        // Given
+        Person person = new Person();
+        Integer expected = 60;
+
+        //When
+        person.setHeightInInches(60);
+
+        //Then
+        Integer actual = person.getHeightInInches();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetWeightInPounds() {
+        // Given
+        Person person = new Person();
+        Integer expected = 120;
+
+        // When
+        person.setWeightInPounds(120);
+
+        // Then
+        Integer actual = person.getWeightInPounds();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetFavoriteFood() {
+        // Given
+        Person person = new Person();
+        String expected = "cereal";
+
+        // When
+        person.setFavoriteFood("cereal");
+
+        // Then
+        String actual = person.getFavoriteFood();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetOccupation() {
+        // Given
+        Person person = new Person();
+        String expected = "Developer";
+
+        // When
+        person.setOccupation("Developer");
+
+        // Then
+        String actual = person.getOccupation();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetEthnicity() {
+        // Given
+        Person person = new Person();
+        String expected = "African American";
+
+        // When
+        person.setEthnicity("African American");
+
+        // Then
+        String actual = person.getEthnicity();
         Assert.assertEquals(expected, actual);
     }
 }
