496bd32d5886e0a77e3a23e7d1011abda1d2fe59
val <valfragier@gmail.com>
2019-10-24 16:51:23 -0400

    Lab Complete

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,14 +6,45 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int height;
+    private String city;
+    private String state;
+    private boolean student;
 
-    public Person() {
+    // constructor
+    public Person(String name, int age,int height, String city, String state, boolean student) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.city = city;
+        this.state = state;
+        this.student = student;
+    }
+
+    public Person(){
+        name = "";
+        age = 25;
+        height = 51;
+        city = "Baltimore";
+        state = "Maryland";
+        student = true;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+    }
+
+    public Person(boolean student,int height) {
+    }
+
+    public Person(String city, String state) {
+    }
+
+    public Person(boolean student){
+
     }
 
     public Person(String name, int age) {
@@ -22,14 +53,46 @@ public class Person {
     public void setName(String name) {
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setAge(int age) {
     }
-
-    public String getName() {
-        return null;
-    }
-
     public Integer getAge() {
-        return null;
+        return age;
     }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public boolean isStudent() {
+        return student;
+    }
+
+    public void setStudent(boolean student) {
+        this.student = student;
+    }
+
 }
