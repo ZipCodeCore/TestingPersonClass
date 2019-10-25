208877541f66bd815252ac0ab191fa90d7c09ad8
mike <mike.schoenbeck@gmail.com>
2019-10-24 22:31:25 -0400

    completed lab

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,40 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
-    private int age;
+    private String name = "Leon";
+    private int age = 5;
 
     public Person() {
+    this.name = "";
+    this.age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
