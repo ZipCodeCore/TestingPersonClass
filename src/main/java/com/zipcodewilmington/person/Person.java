83f1bfe64df7f4449b916610a2de3f6d689ed616
Mike King <michkin32@gmail.com>
2019-10-26 14:45:49 -0400

    Part one of instructions done

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,49 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
+    private String name = "";
     private int age;
+    private Double height;
+    private String eyeColor;
+    private String hairColor;
+    private Integer amountOfToes;
+    private ;
 
     public Person() {
+        age = Integer.MAX_VALUE;
     }
 
-    public Person(int age) {
+    public Person(int givenAge) {
+        age = givenAge;
+
     }
 
-    public Person(String name) {
+    public Person(String givenName) {
+        name = givenName;
+
     }
 
-    public Person(String name, int age) {
+    public Person(String givenName, int givenAge) {
+        name = givenName;
+        age = givenAge;
+
     }
 
-    public void setName(String name) {
+    public void setName(String givenName) {
+        name = givenName;
     }
 
-    public void setAge(int age) {
+    public void setAge(int givenAge) {
+        age = givenAge;
+
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return (Integer)age;
     }
 }
