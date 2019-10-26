b5d1538d9f52b0982b1fa3a064bd14f86c27c3f5
michael-doll <MWDoll88@gmail.com>
2019-10-25 22:18:04 -0400

    add five methods and add tests

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -2,34 +2,62 @@ package com.zipcodewilmington.person;
 
 /**
  * Created by leon on 2/12/18.
+ * Michael Doll 10.25.2019
  */
 public class Person {
     private String name;
+    private String lastName;
     private int age;
+    private int arms;
+    private int legs;
+    private int eyes;
+    private String favoriteCereal;
 
-    public Person() {
-    }
+    public Person() {}
 
-    public Person(int age) {
-    }
+    public Person(int age) { this.age = age; }
 
-    public Person(String name) {
-    }
+    public Person(String name) { this.name = name ; }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
+    public Person(String lastName, int arms, int legs, int eyes, String favoriteCereal){
+        this.lastName = lastName;
+        this.arms = arms;
+        this.legs = legs;
+        this.eyes = eyes;
+        this.favoriteCereal = favoriteCereal;
+    }
+    public Person(String name, int age, String lastName, int arms, int legs, int eyes, String favoriteCereal){
+        this.name = name;
+        this.age = age;
+        this.lastName = lastName;
+        this.arms = arms;
+        this.legs = legs;
+        this.eyes = eyes;
+        this.favoriteCereal = favoriteCereal;
+    }
+    public void setName(String name) {this.name = name;}
 
-    public void setName(String name) {
-    }
-
-    public void setAge(int age) {
-    }
+    public void setAge(int age) { this.age = age;}
+    public void setLastName(String lastName){this.lastName = lastName;}
+    public void setArms(int arms){this.arms = arms; }
+    public void setLegs(int legs){this.legs = legs; }
+    public void setEyes(int eyes){this.eyes = eyes; }
+    public void setFavoriteCereal(String favoriteCereal){this.favoriteCereal = favoriteCereal; }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
+    public String getLastName(){return lastName;}
+    public Integer getArms(){return arms;}
+    public Integer getLegs(){return legs;}
+    public Integer getEyes(){return eyes;}
+    public String getFavoriteCereal(){return favoriteCereal;}
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -3,6 +3,8 @@ package com.zipcodewilmington.person;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.swing.table.TableColumn;
+
 /**
  * Created by leon on 2/12/18.
  */
@@ -10,12 +12,10 @@ public class TestPerson {
     @Test
     public void testDefaultConstructor() {
         // Given
-        String expectedName = "";
-        Integer expectedAge = Integer.MAX_VALUE;
-
+        String expectedName = null;
+        Integer expectedAge = 0;
         // When
         Person person = new Person();
-
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
@@ -28,10 +28,8 @@ public class TestPerson {
     public void testConstructorWithName() {
         // Given
         String expected = "Leon";
-
         // When
         Person person = new Person(expected);
-
         // Then
         String actual = person.getName();
         Assert.assertEquals(expected, actual);
@@ -41,10 +39,8 @@ public class TestPerson {
     public void testConstructorWithAge() {
         // Given
         Integer expected = 5;
-
         // When
         Person person = new Person(expected);
-
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
@@ -56,10 +52,8 @@ public class TestPerson {
         // Given
         Integer expectedAge = 5;
         String expectedName = "Leon";
-
         // When
         Person person = new Person(expectedName, expectedAge);
-
         // Then
         Integer actualAge = person.getAge();
         String actualName = person.getName();
@@ -73,11 +67,9 @@ public class TestPerson {
         // Given
         Person person = new Person();
         String expected = "Leon";
-
         // When
         person.setName(expected);
         String actual = person.getName();
-
         // Then
         Assert.assertEquals(expected, actual);
     }
@@ -87,12 +79,69 @@ public class TestPerson {
         // Given
         Person person = new Person();
         Integer expected = 5;
-
         // When
         person.setAge(expected);
-
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetLastName() {
+        // Given
+        Person person = new Person();
+        String expected = "Taco";
+        // When
+        person.setLastName(expected);
+        // Then
+        String actual = person.getLastName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetArms(){
+        //Given
+        Person person = new Person();
+        Integer expected = 2;
+        //When
+        person.setArms(expected);
+        //Then
+        Integer actual = person.getArms();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetLegs(){
+        //Given
+        Person person = new Person();
+        Integer expected = 2;
+        //When
+        person.setLegs(expected);
+        //Then
+        Integer actual = person.getLegs();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetEyes(){
+        //Given
+        Person person = new Person();
+        Integer expected = 2;
+        //When
+        person.setEyes(expected);
+        //Then
+        Integer actual = person.getEyes();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetFavoriteCereal(){
+        //Given
+        Person person = new Person();
+        String expected = "Cookie Crisp ";
+        //When
+        person.setFavoriteCereal(expected);
+        //Then
+        String actual = person.getFavoriteCereal();
         Assert.assertEquals(expected, actual);
     }
 }
