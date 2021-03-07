da746ef1156e53f3f9db00d87d2d61ff8cfd2cda
smartist17 <79124739+smartist17@users.noreply.github.com>
2021-03-07 17:40:15 -0500

    Person lab complete

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -8,28 +8,40 @@ public class Person {
     private int age;
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
     }
 
-    public Person(int age) {
+    public Person(int personAge) {
+        age = personAge;
+
     }
 
-    public Person(String name) {
+    public Person(String nameOfPerson) {
+        name = nameOfPerson;
     }
 
-    public Person(String name, int age) {
+    public Person(String nameOfPerson, int ageOfPerson) {
+        name = nameOfPerson;
+        age = ageOfPerson;
     }
 
-    public void setName(String name) {
+    public void setName(String nameOfPerson) {
+        name = nameOfPerson;
+
     }
 
-    public void setAge(int age) {
+    public void setAge(int ageOfPerson) {
+        age = ageOfPerson;
     }
 
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return age;
     }
 }
