99c3df98a245bf098ea1d3c3fb5e34b2bc24565c
Nick <nick@zipcodes-mbp-78.myfiosgateway.com>
2021-07-01 17:03:30 -0400

    Finished the lab, completing all test cases

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -11,25 +11,31 @@ public class Person {
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
