331310fa26e469744c819b9331eb7c91bc004b31
Rex <rex@zipcodes-mbp-51.lan>
2021-07-09 04:24:19 -0400

    finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,83 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String facialHair;
+    private String ethnicity;
+    private String glasses;
+    private int heightInches;
+    private int weightLbs;
 
     public Person() {
+        this.name = "";
+        this.age = 2147483647;     //max number 2.147b
     }
 
     public Person(int age) {
+        setAge(age);
     }
 
     public Person(String name) {
+        setName(name);
     }
 
     public Person(String name, int age) {
+        setName(name);
+        setAge(age);
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
+
+    public String setFacialHair() {
+        return facialHair;
+    }
+
+    public void setFacialHair(String facialHair) {
+        this.facialHair = facialHair;
+    }
+
+    public String setEthnicity() {
+        return ethnicity;
+    }
+
+    public void setEthnicity(String ethnicity) {
+        this.ethnicity = ethnicity;
+    }
+
+    public String setGlasses() {
+        return glasses;
+    }
+
+    public void setGlasses(String glasses) {
+        this.glasses = glasses;
+    }
+    public int setHeightInches() {
+        return heightInches;
+    }
+
+    public void setHeightInches(int heightInches) {
+        this.heightInches = heightInches;
+    }
+
+    public int setWeightLbs() {
+        return weightLbs;
+    }
+
+    public void setWeightLbs(int weightLbs) {
+        this.weightLbs = weightLbs;
+    }
+
 }
