a1dc1665de36dac44346bf708eb4705d96653991
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-11-04 10:41:44 -0400

    All tests pass

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -26,9 +26,11 @@ public class Person {
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
